706608eddf7871574ebea4856928ce1b27a3397c
Aliyu Adamu Ahmad <95003138+aliyubasug@users.noreply.github.com>
2021-12-01 00:13:43 +0100

    Delete Script_JibrinAbdullahi.java

--- Script_JibrinAbdullahi.java
+++
@@ -1,8 +0,0 @@
-public class jibrin abdullahi uba JavaScript{
-	public static void main(String[]args){
-		System.out.println("Name: jibrin abdullahi uba");
-		System.out.println("Email: jibrinabdullahiuba@gmail.com");
-		System.out.println("Slack id: @jibrinabdullahiuba");
-		System.out.println("Area of interest: DNA sequencing and technology");
-	}
-}
