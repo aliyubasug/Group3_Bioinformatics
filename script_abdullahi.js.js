c1ca532b3632807fbaf840ea764e034fd6c33081
Aliyu Adamu Ahmad <95003138+aliyubasug@users.noreply.github.com>
2021-12-01 16:06:43 +0100

    Delete script_abdullahi.js.js

--- script_abdullahi.js.js
+++
@@ -1,5 +0,0 @@
-alert('Name: abdullahi Dahiru');
-alert('Email: dahiruabdullahi0082@gmail.com');
-alert('Area of interest: Genomics');
-alert('Github: @abdullahi0082');
-alert('Slack ID: @abdullahidahiru');
