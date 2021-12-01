1fc29e68e2bf809f7ffc6b48ac63bf08fd016ed5
Ibrahim Bello <95057016+Ibrahim-Bello@users.noreply.github.com>
2021-12-01 10:52:50 +0100

    Add files via upload

---
+++ Script_ibrahimbelloclanguage.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*Programme to print out my details
+    Written by:
+        IBRAHIM BELLO
+        BASUG/UG/BHS/ANT/ANA/18/053
+
+    */
+
+
+
+    printf("BIO INFORMATICS");
+    printf("NAME: IBRAHIM BELLO\n");
+    printf("EMAIL: ibrahim.bello6961@gmail.com\n");
+    printf("SLACK USERNAME: @Ibrahim Bello\n");
+    printf("GITHUB USERNAME: ibrahim-bello\n");
+    printf("AREA OF INTEREST: GENOMICS\n");
+    return 0;
+}
