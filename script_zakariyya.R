899b46a85a524a068c9931e7a8414eb2886808b6
Zakariyyauba <95166994+Zakariyyauba@users.noreply.github.com>
2021-12-01 21:10:36 +0100

    Add files via upload

---
+++ script_zakariyya.R
@@ -0,0 +1,21 @@
+getwd()
+
+# NAME
+name<- c('Zakariyya M Umar')
+print(name)
+
+# EMAIL ADDRESS
+email<- c('mumarzakariyya@gmail.com')
+print(email)
+
+# SLACK USERNAME
+slack<- c('@zakariyyamumar')
+print(slack)
+
+# BIOINFORMATIC AREA OF INTEREST
+bioinformatic('genomic')
+print(bioinformatic)
+
+# DATA FRAME
+rscript<- data.frame(name, email, slack, bioinformatic)
+rscript
