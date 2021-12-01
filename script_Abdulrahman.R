a2d88654742ee998b3c198a253b24f42b8ac0f33
LAWAL ABDULRAHMAN KHALIL <95120615+Marwablk@users.noreply.github.com>
2021-12-01 18:29:50 +0100

    Add files via upload

---
+++ script_Abdulrahman.R
@@ -0,0 +1,17 @@
+getwd()
+
+# NAME
+name<- c('Abdulrahman Khalil')
+name
+
+# EMAIL ADDRESS
+email<- c('marpheed@gmail.com')
+email
+
+# AREA OF INTEREST 
+bioinformatic<- ('genomics')
+bioinformatic
+
+# SLACK ID
+slack<- c('@abdulrahmanlkhalil')
+# RSCRIPT
