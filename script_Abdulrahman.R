4fa8e46803ecc5208023c6bff2ecd91b105b9c1d
Aliyu Adamu Ahmad <95003138+aliyubasug@users.noreply.github.com>
2021-12-01 23:48:24 +0100

    Update script_Abdulrahman.R

--- script_Abdulrahman.R
+++ script_Abdulrahman.R
@@ -1,17 +1,11 @@
-getwd()
+#!/usr/local/bin/Rscript
 
-# NAME
+
 name<- c('Abdulrahman Khalil')
-name
-
-# EMAIL ADDRESS
 email<- c('marpheed@gmail.com')
-email
-
 # AREA OF INTEREST 
 bioinformatic<- ('genomics')
 bioinformatic
-
 # SLACK ID
 slack<- c('@abdulrahmanlkhalil')
 # RSCRIPT
