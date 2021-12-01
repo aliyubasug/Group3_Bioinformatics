3a71ef15666cdb0b3e846120133c6214da0ff091
Aliyu Adamu Ahmad <95003138+aliyubasug@users.noreply.github.com>
2021-12-02 00:35:40 +0100

    This Is The Bash Script That clones The Git Reposi

---
+++ Group3repo.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Now im Running A command that Will clone The group Git rpository
+
+git clone https://github.com/aliyubasug/Group3_Bioinformatics.git
+
+# im changing my working directory 
+
+cd ./Group3_Bioinformatics.git
+
+#Im now asking to generate a csv file 
+
+chmod +x script*
+
+for i in $(ls script*)
+do
+	./$i | awk -F ',' '{OFS="\t";print $1,$2,$3,$4,$5}' >> Group3repo.csv
+
+done
+
+# The files for group3 is sucsessfully extracted
+
+
+
