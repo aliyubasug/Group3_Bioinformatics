c330654ed4a518acf4402d7308607d1b75834032
Aliyu Adamu Ahmad <95003138+aliyubasug@users.noreply.github.com>
2021-12-02 20:55:43 +0100

    Update and rename Script_ibrahimbelloclanguage.rb to Script_ibrahimbell.php

---
+++ Script_ibrahimbell.php
@@ -0,0 +1,3 @@
+/usr/bin/php
+
+Echo "IBRAHIM BELLO,ibrahimbello6961@gmail.com, @Ibrahim Bello,GENOMICS"
--- Script_ibrahimbelloclanguage.rb
+++
@@ -1,3 +0,0 @@
-#/usr/bin/Ruby
-
-Echo"IBRAHIM BELLO,ibrahim.bello6961@gmail.com, @Ibrahim Bello,GENOMICS"
