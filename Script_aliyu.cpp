71112de49dce6c9f6357b8cff3264a66d9cefc52
Aliyu Adamu Ahmad <95003138+aliyubasug@users.noreply.github.com>
2021-12-02 08:04:53 +0100

    Update Script_aliyu.cpp

--- Script_aliyu.cpp
+++ Script_aliyu.cpp
@@ -1,13 +1,4 @@
-#include<iostream>
-using namespace std;
+#!/usr/bin/env bash
 
-int main()
-{
-    cout << "-------------------BIOINFORMATICS C++ SCRIPT----------------\n";
-    cout << "\t\tHello Aliyu Muhammad:\n\n";
-    cout << "Name:"<< "\t\t\t" << "Aliyu Muhammad\n";
-    cout << "Email:"<< "\t\t\t" << "aliyuhammad1@gmail.com\n";
-    cout << "Slack_Id:" << "\t\t" << "@Aliyu_Muhammad\n";
-    cout << "Area Of Interest:" << "\t" << "Genomics\n";
-    return 0;
-}
+Echo "Aliyu Muhammad,aliyuhammad1@gmail.com,@Aliyu_Muhammad,Genomics\n"
+
