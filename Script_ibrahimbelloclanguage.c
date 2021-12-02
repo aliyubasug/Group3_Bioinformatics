01a78407d29d7429d1f7a31e4d70e587f4b96001
Aliyu Adamu Ahmad <95003138+aliyubasug@users.noreply.github.com>
2021-12-02 08:10:43 +0100

    Update Script_ibrahimbelloclanguage.c

--- Script_ibrahimbelloclanguage.c
+++ Script_ibrahimbelloclanguage.c
@@ -1,22 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
+#/usr/bin/env bash
 
-int main()
-{
-    /*Programme to print out my details
-    Written by:
-        IBRAHIM BELLO
-        BASUG/UG/BHS/ANT/ANA/18/053
-
-    */
-
-
-
-    printf("BIO INFORMATICS");
-    printf("NAME: IBRAHIM BELLO\n");
-    printf("EMAIL: ibrahim.bello6961@gmail.com\n");
-    printf("SLACK USERNAME: @Ibrahim Bello\n");
-    printf("GITHUB USERNAME: ibrahim-bello\n");
-    printf("AREA OF INTEREST: GENOMICS\n");
-    return 0;
-}
+Echo"IBRAHIM BELLO,ibrahim.bello6961@gmail.com, @Ibrahim Bello,GENOMICS"
