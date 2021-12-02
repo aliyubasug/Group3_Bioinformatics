35c57fd23ff3beabc4f11be1813e4421fdcca971
Aliyu Adamu Ahmad <95003138+aliyubasug@users.noreply.github.com>
2021-12-02 08:13:28 +0100

    Update Script_idris.php

--- Script_idris.php
+++ Script_idris.php
@@ -1,16 +1,5 @@
-<html>
-<title> Bioinformatic Assignment </title>
-<?php 
+#!/usr/bin/env bash
 
-$_name = 'Idris  Abdurrashid';
-$_Slack_id = '@idris.Abdurrashid';
-$_email = 'Idrisabdurrashid9@gmail.com';
-$_areaofinterest = 'System Biology';
+Echo "Idris  Abdurrashid, @idris.Abdurrashid,Idrisabdurrashid9@gmail.com,System Biology"
 
-
-echo  '<p><h3>My Name is ',$_name, '</p></h3>';
-echo '<p><h3> My Slack Id is ',$_Slack_id, '</p></h3';
-echo  '<p><h3> My Email Address is ',$_email, '</p></h3';
-echo '<p><h3> I am very Interested in the field of  ',$_areaofinterest, '</p></h3';
-?>
-</html>
+#done
