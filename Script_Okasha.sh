e2a85e2efbe43c2a9a7c8514a36ebf3fdb270cf7
Aliyu Adamu Ahmad <95003138+aliyubasug@users.noreply.github.com>
2021-12-02 07:45:48 +0100

    Update Script_Okasha.sh

--- Script_Okasha.sh
+++ Script_Okasha.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-#NAME: Okasha Ibrahim Bako
-#EMAIL: okashaibrahimbako01@gmail.com
-#SLACK USER: @Okasha_Ibrahim_bako
-#AREA OF INTREST: Genomics
+#!/use/bin/bash
+
+Echo Okasha Ibrahim Bako,okashaibrahimbako01@gmail.com,@Okasha_Ibrahim_bako, Genomics
 
 #END
 
