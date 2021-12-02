b77b444b5cc0075ee46093f115f9827e51b84eca
Aliyu Adamu Ahmad <95003138+aliyubasug@users.noreply.github.com>
2021-12-02 08:30:21 +0100

    Update script_suraja.py

--- script_suraja.py
+++ script_suraja.py
@@ -1,8 +1,5 @@
-#<-----------suraja Project------------>
-print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
+#!/usr/bin/env bash
 
-print("\t\tHello suraja Ibrahim, below are your informations:\n")
-print("Name: " + "\t\t\t" + "Suraja Ibrahim")
-print("Email: " + "\t\t\t"+ "surajaibbrahim724@gmail.com")
-print("Slack_username: " + "\t" + "@Suraja_Ibrahim")
-print("Area of interest: " + "\t" + "genomics")
+Echo "suraja Ibrahim,Suraja Ibrahim,surajaibbrahim724@gmail.com,genomics"
+
+#my details using Phyton
