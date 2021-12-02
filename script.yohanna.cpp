8eef8760d2b5133d8e170fd52aab3701c5f3f958
Aliyu Adamu Ahmad <95003138+aliyubasug@users.noreply.github.com>
2021-12-02 09:06:22 +0100

    Update script.yohanna.cpp

--- script.yohanna.cpp
+++ script.yohanna.cpp
@@ -1,13 +1,4 @@
-#include<iostream>
-using namespace std;
+#!/usr/bin/env bash
 
-int main()
-{
-    cout << "-------------------BIOINFORMATICS C++ SCRIPT----------------\n";
-    cout << "\t\tHello:\n\n";
-    cout << "Name:"<< "\t\t\t" << "Yohanna Apolmi \n";
-    cout << "Email:"<< "\t\t\t" << "yohannapyrex88@gmail.com\n";
-    cout << "Slack_Id:" << "\t\t" << "@yohanna apolmi_ \n";
-    cout << "Area Of Interest:" << "\t" << "Proteomics \n";
-    return 0;
-}
+Echo  "Yohanna Apolmi,yohannapyrex88@gmail.com,@yohanna apolmi,Proteomics"
+
